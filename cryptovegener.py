70b533c87c147d31e9d7048d0194c0f56cd8271d
naxomi <46137424+naxomi@users.noreply.github.com>
2018-12-27 18:41:59 +0100

    Update cryptovegener.py

--- cryptovegener.py
+++ cryptovegener.py
@@ -5,7 +5,7 @@ alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
             "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","à","ç","é","è","ê",
             "ë","â","ä","ù","û","ü","ô","ö","ÿ","ù","ò","ì","î","ï",",",":",";",".","?",
             "!","/","+","=","'","(",")"," ","-","0","1","2","3","4","5","6","7","8","9"]
-dicoAlphabet = {0 : ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
+dictionnaryAlphabet = {0 : ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
                      "t","u","v","w","x","y","z", "A","B","C","D","E","F","G","H","I","J","K","L",
                      "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","à","ç","é","è","ê",
                      "ë","â","ä","ù","û","ü","ô","ö","ÿ","ù","ò","ì","î","ï",",",":",";",".","?",
@@ -15,66 +15,66 @@ dicoAlphabet = {0 : ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"
 for loop in range(1, 95):
     alphabet.append(alphabet[0])
     del alphabet[0]
-    dicoAlphabet[loop] = alphabet[:]
+    dictionnaryAlphabet[loop] = alphabet[:]
 alphabet.append(alphabet[0])
 del alphabet[0]
 
 
 #Function to encrypt
-def encrypt(clef, motACrypter):
+def encrypt(key, wordToCrypt):
     #Create a list of the message to encrypt
-    listeMotACrypter = [lettre for index, lettre in enumerate(motACrypter)]
+    listWordToCrypt = [letter for index, letter in enumerate(wordToCrypt)]
     
     #Adjust the length of the key to the length of the message
-    listeClef = []
-    boucle = True
-    while boucle:
-        for loop in range(len(clef)):
-            listeClef.append(clef[loop])
-            if len(listeMotACrypter) == len(listeClef):
-                boucle = False
+    listKey = []
+    length = True
+    while length:
+        for loop in range(len(key)):
+            listKey.append(key[loop])
+            if len(listWordToCrypt) == len(listKey):
+                length = False
                 break   
     
     #Find the indexs of the letters from the message         
-    listeIndexMotACrypter = [loop2 for loop1 in range(len(listeMotACrypter)) for loop2 in range(len(alphabet)) if listeMotACrypter[loop1] == alphabet[loop2]]
+    listIndexWordToCrypt = [loop2 for loop1 in range(len(listWordToCrypt)) for loop2 in range(len(alphabet)) if listWordToCrypt[loop1] == alphabet[loop2]]
     
     #Find the indexs of the letters from the key  
-    listeIndexVraiClef = [loop2 for loop1 in range(len(listeClef)) for loop2 in range(len(alphabet)) if listeClef[loop1] == alphabet[loop2]]
+    listIndexKey = [loop2 for loop1 in range(len(listKey)) for loop2 in range(len(alphabet)) if listKey[loop1] == alphabet[loop2]]
     
     #Use indexs find above to crypt each character of the message   
-    listeMotCrypter = [dicoAlphabet[listeIndexVraiClef[loop]][listeIndexMotACrypter[loop]] for loop in range(len(listeMotACrypter))]
+    listCryptedWord = [dictionnaryAlphabet[listIndexKey[loop]][listIndexWordToCrypt[loop]] for loop in range(len(listWordToCrypt))]
     
-    motCrypter = "".join(listeMotCrypter)
-    print(motCrypter, "    is the crypted message")
+    cryptedWord = "".join(listCryptedWord)
+    print(cryptedWord, "    is the crypted message")
     
     
 #Function to decrypt
-def decrypt(clef, motADecrypter):
+def decrypt(key, wordToDecrypt):
     #Create a list of the message to decrypt
-    listeMotADecrypter = [lettre for index, lettre in enumerate(motADecrypter)]
+    listWordToDecrypt = [letter for index, letter in enumerate(wordToDecrypt)]
        
     #Adjust the length of the key to the length of the message
-    listeClef = []
-    boucle = True
-    while boucle:
-        for loop in range(len(clef)):
-            listeClef.append(clef[loop])
-            if len(listeMotADecrypter) == len(listeClef):
-                boucle = False
+    listKey = []
+    length = True
+    while length:
+        for loop in range(len(key)):
+            listKey.append(key[loop])
+            if len(listWordToDecrypt) == len(listKey):
+                length = False
                 break
     
     #Find the indexs of the letters from the key  
-    listeIndexVraiClef = [loop2 for loop1 in range(len(listeClef)) for loop2 in range(len(alphabet)) if listeClef[loop1] == alphabet[loop2]]
+    listIndexKey = [loop2 for loop1 in range(len(listKey)) for loop2 in range(len(alphabet)) if listKey[loop1] == alphabet[loop2]]
     
     #Find the indexs of the decrypted letters
-    listeIndexMotDecrypter = [loop2 for loop1 in range(len(listeMotADecrypter)) for loop2 in range(len(alphabet)) if dicoAlphabet[loop2][listeIndexVraiClef[loop1]] == listeMotADecrypter[loop1]]
+    listIndexWordToDecrypt = [loop2 for loop1 in range(len(listWordToDecrypt)) for loop2 in range(len(alphabet)) if dictionnaryAlphabet[loop2][listIndexKey[loop1]] == listWordToDecrypt[loop1]]
     
     #Create a list of all the letters decrypted
-    listeMotDecrypter = [alphabet[listeIndexMotDecrypter[loop]] for loop in range(len(listeIndexMotDecrypter))]
+    listWordDecrypted = [alphabet[listIndexWordToDecrypt[loop]] for loop in range(len(listIndexWordToDecrypt))]
     
     #Create the decrypted message from the list
-    motDecrypter = "".join(listeMotDecrypter)
-    print(motDecrypter, "    is the decrypted message")
+    wordDecrypted = "".join(listWordDecrypted)
+    print(wordDecrypted, "    is the decrypted message")
   
 reponse = str(input("Would you like to encrypt or decrypt : (e/d)\n"))  
 if reponse == "e":      
